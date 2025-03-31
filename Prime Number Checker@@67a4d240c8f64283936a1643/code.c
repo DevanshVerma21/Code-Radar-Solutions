45a09b0addcb1efaaebc620545260d3f85ace90f
Devansh Verma <vermadevansh0001@gmail.com>
2025-03-31 20:02:51 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,13 +1,20 @@
-#include <math.h>
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;  // Numbers <= 1 are not prime
+    }
+    if (num == 2 || num == 3) {
+        return 1;  // 2 and 3 are prime
+    }
+    if (num % 2 == 0 || num % 3 == 0) {
+        return 0;  // Eliminate even numbers and multiples of 3
+    }
 
-int isPrime(int num){
-    if(num<=1){
-        return 0;
-    }
-    for(int i = 2;i*i<=(num);i++){
-        if(num % i == 0){
-            return 0;
+    // Check divisibility from 5 onwards, skipping even numbers
+    for (int i = 5; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            return 0;  // Found a divisor, not prime
         }
-        return 1;
     }
+
+    return 1;  // Prime number
 }
